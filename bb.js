b153ebc470c6ec60bb886ae46b34b0074a7a3ae0
Ingelbert Figueroa <ingelbert.figueroa@gateb.com>
2021-08-18 14:27:20 -0700

    latest changes

--- bb.js
+++ bb.js
@@ -1,5 +1,5 @@
 const puppeteer = require('puppeteer');
-let url= '';
+let url = process.env.BB_URL;
 
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config();
@@ -10,16 +10,31 @@ if (process.env.NODE_ENV !== 'production') {
   const 
     browser = await puppeteer.launch({headless: false}),
     page = await browser.newPage();
-  
-    await page.goto(test);
-    await page.waitForSelector('button.btn-primary');
-    await page.click('button.btn-primary');
-    await page.waitForSelector('div.success');
-    await page.goto(process.env.BB_CART);
-    await page.waitForSelector('button.btn-primary');
-    await page.click('button.btn-primary');
-    await page.waitForSelector('input[type="email"]');
-    await page.type('input[type="email"]',process.env.BB_EMAIL);
-    await page.type('input[type="password"]',process.env.BB_PASSWORD);
-    await page.click('button.btn-secondary[type="submit"]');
-  })();
+  while (true) {
+    try {
+      console.log('attempting');
+      await page.goto(url,{timeout: 6000});
+      await page.click('button.btn-primary');
+      await page.waitForSelector('div.success',{timeout: 3000});
+      break;
+    }
+    catch(e) {
+      continue;
+    }
+  }
+  console.log('checking out');
+  await page.goto(process.env.BB_CART);
+  await page.waitForSelector('button.btn-primary');
+  await page.click('button.btn-primary');
+  await page.waitForSelector('input[type="email"]');
+  await page.type('input[type="email"]',process.env.BB_EMAIL);
+  await page.type('input[type="password"]',process.env.BB_PASSWORD);
+  await page.click('button.btn-secondary[type="submit"]');
+  await page.waitForNavigation();
+  await page.waitForSelector('button.button__fast-track');
+  await page.click('button.button__fast-track');
+})();
+
+//document.querySelector('div#attach-desktop-sideSheet');
+//document.querySelector('input#add-to-cart-button').click();
+//document.querySelector('input[aria-labelledby="attachSiNoCoverage-announce"]');
