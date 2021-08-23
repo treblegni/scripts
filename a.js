552b7350aedad8f4348483fa0eba7769f58c7379
treblegni <ingelbert.figueroa@gmail.com>
2021-08-22 17:04:02 -0700

    a updated

--- a.js
+++ a.js
@@ -1,4 +1,5 @@
 const puppeteer = require('puppeteer');
+const { createWorker } = require('tesseract.js');
 let url = process.env.A_URL;
 
 if (process.env.NODE_ENV !== 'production') {
@@ -6,48 +7,53 @@ if (process.env.NODE_ENV !== 'production') {
   url = process.env.A_URL;
 }
 
-const resolveImage = async (imgurl) => {
-  const { createWorker } = require('tesseract.js');
-  const worker = createWorker();
-
-  await worker.load();
-  await worker.loadLanguage('eng');
-  await worker.initialize('eng');
-  const { data: { text } } = await worker.recognize(imgurl);
-  await worker.terminate();
-  return text;
-};
-
 (async () => {
   const 
     browser = await puppeteer.launch({headless: false}),
     page = await browser.newPage();
+  await page.exposeFunction('resolveImage',async (imgurl) => {
+    const worker = createWorker();
+
+    await worker.load();
+    await worker.loadLanguage('eng');
+    await worker.initialize('eng');
+    const { data: { text } } = await worker.recognize(imgurl);
+    await worker.terminate();
+    return text;
+  });
 
   let error = '';
 
   while (true) {
     try {
       console.log('attempting');
-      await page.goto(url,{timeout: 6000});
-      if (error.includes('input#add-to-cart-button')) {
-        const captcha = await page.$('input#captchacharacters');
+      await page.goto(url,{timeout: 5000});
+      const captcha = await page.$('input#captchacharacters');
         
-        if (captcha) {
-          const
-            textElement = await page.$('img'),
-            text = await page.evaluate(async (el) => {
-              return await resolveImage(el.src);
-            },textElement);
+      if (captcha) {
+        const textElement = await page.$('img');
+        const text = await page.evaluate(async (el) => {
+          return await resolveImage(el.src);
+        },textElement);
+        if (text) {
           await page.type('input#captchacharacters',text);
-          await page.click('');
-          await page.waitForTimeout(2000);
+          await page.click('button[type="submit"]');
         }
       }
+      // await page.waitForNavigation();
+      // await page.waitForTimeout(1000);
+      await page.waitForSelector('input#add-to-cart-button',{timeout: 1000});
       await page.click('input#add-to-cart-button');
-      await page.waitForTimeout(10000);
-      await page.click('input[aria-labelledby="attachSiNoCoverage-announce"]');
-      await page.waitForTimeout(2000);
-      await page.click('input[aria-labelledby="attach-sidesheet-checkout-button-announce"]');
+      const insurance = await page.$('input[aria-labelledby="attachSiNoCoverage-announce"]');
+      if (insurance) {
+        await page.waitForTimeout(1000);
+        await page.click('input[aria-labelledby="attachSiNoCoverage-announce"]');
+        const checkout = await page.$('input[aria-labelledby="attach-sidesheet-checkout-button-announce"]');
+        if (checkout) {
+          await page.click('input[aria-labelledby="attach-sidesheet-checkout-button-announce"]');
+        }
+      }
+      await page.waitForNavigation();
       break;
     }
     catch(e) {
@@ -56,13 +62,15 @@ const resolveImage = async (imgurl) => {
     }
   }
   console.log('checking out');
-  // await page.waitForSelector('input[type="email"]');
-  // await page.type('input[type="email"]',process.env.A_EMAIL);
-  // await page.click('input#continue');
-  // await page.waitForSelector('input[type="password"]');
-  // await page.type('input[type="password"]',process.env.A_PASSWORD);
-  // await page.click('input#signInSubmit');
-  // await page.waitForNavigation();
-  // await page.waitForSelector('input[aria-labelledby="bottomSubmitOrderButtonId-announce"]');
-  // await page.click('input[aria-labelledby="bottomSubmitOrderButtonId-announce"]');
+  await page.waitForSelector('a#hlb-ptc-btn-native');
+  await page.click('a#hlb-ptc-btn-native');
+  await page.waitForSelector('input[type="email"]');
+  await page.type('input[type="email"]',process.env.A_EMAIL);
+  await page.click('input#continue');
+  await page.waitForSelector('input[type="password"]');
+  await page.type('input[type="password"]',process.env.A_PASSWORD);
+  await page.click('input#signInSubmit');
+  await page.waitForNavigation();
+  await page.waitForSelector('input[aria-labelledby="bottomSubmitOrderButtonId-announce"]');
+  await page.click('input[aria-labelledby="bottomSubmitOrderButtonId-announce"]');
 })();
